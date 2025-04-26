58b1ea3a43554c80eae4965f4c78e988daedf495
Vi-user <vi.volkova.2712@gmail.com>
2025-04-27 00:25:47 +0300

    feat: add hash block

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { createHash } from 'node:crypto';
+import fsAsync from "node:fs/promises";
+import { stdout } from 'node:process';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+  const data = await fsAsync.readFile('./files/fileToCalculateHashFor.txt', {encoding: 'utf-8'});
+
+  const hash = createHash('sha256')
+    .update(data, 'utf8')
+    .digest('hex');
+  stdout.write(hash);
 };
 
 await calculateHash();
