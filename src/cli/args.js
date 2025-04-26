27b72d4ef3709e2320b637327913b84ca43b9eaa
Vi-user <vi.volkova.2712@gmail.com>
2025-04-27 00:22:02 +0300

    feat: add cli block

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+  const params = Object.entries(process.argv).slice(2);
+  let res = [];
+  for (let i = 0; i < params.length; i += 2) {
+    res.push(`${params[i][1].slice(2)} is ${params[i+1][1]}`)
+  }
+
+  console.log(res.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+  let res = [];
+  for (const [key, value] of Object.entries(process.env)) {
+
+    if (key.startsWith('RSS_')) {
+      res.push(`${key}=${value}`)
+    }
+
+  }
+  console.log(res.join('; '));
 };
 
 parseEnv();
