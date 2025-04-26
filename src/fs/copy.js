b4432b74edd6c9395b1c954b166735d241582a5e
Vi-user <vi.volkova.2712@gmail.com>
2025-04-26 23:36:44 +0300

    init: start nodejs-basics task. Add fs block

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import fsAsync from 'node:fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+  const errorText = 'FS operation failed';
+  const originalFolder = './files';
+  const copyFolder = './files_copy';
+  try {
+    if (!fs.existsSync(originalFolder) || fs.existsSync(copyFolder)) {
+      throw new Error(errorText);
+    } else {
+      fs.mkdirSync(copyFolder);
+      const curFolderFiles = await fsAsync.readdir(originalFolder);
+      curFolderFiles.forEach(file => {
+        const curFile = path.join(originalFolder, file);
+        const newFile = path.join(copyFolder, file);
+        fsAsync.copyFile(curFile, newFile);
+      })
+    }
+  } catch (e) {
+    console.log(e)
+  }
 };
 
 await copy();
+
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+    // Write your code here
+  try {
+    if (!fs.existsSync('./files/wrongFilename.txt') || fs.existsSync('./files/properFilename.md')) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.renameSync('./files/wrongFilename.txt', './files/properFilename.md')
+    }
+  } catch (e) {
+    console.log(e)
+  }
 };
 
 await rename();
