1b68e654bd28aedd73cee9c82c9393d8255dd457
Vi-user <vi.volkova.2712@gmail.com>
2025-04-29 00:55:43 +0300

    feat: add wt block

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from 'node:os'
+import {parentPort, Worker} from "node:worker_threads";
+
 const performCalculations = async () => {
     // Write your code here
+  const cpuNum = os.cpus().length;
+  const minNum = 10;
+
+  const arr = [];
+  for (let i = 0; i < cpuNum; i++) {
+    const worker = new Worker('./worker.js', {workerData: {num: minNum + i }});
+    worker.on('message', (res) => {
+      if (typeof res === "number") {
+        arr.push({'resolved': `${res}`})
+      } else {
+        arr.push({'error': 'null'})
+      }
+
+    });
+  }
+  setTimeout(() => {
+    console.log(arr.sort((a, b) => (+a.resolved < +b.resolved ) ? -1 : 1 ));
+  }, 300);
+
+
 };
 
 await performCalculations();
