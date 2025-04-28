362ee98f0fe4398de0eeadeffde5dd83640e1c0b
Vi-user <vi.volkova.2712@gmail.com>
2025-04-28 22:35:41 +0300

    feat: add zip block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
+import zlib from "node:zlib";
+// import {pipeline} from 'node:stream';
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+  const gzip = zlib.createGzip();
+  const data = fs.createReadStream('./files/fileToCompress.txt');
+  const res = fs.createWriteStream('./files/archive.gz');
+
+  // pipeline(data, gzip, res);
+  data.pipe(gzip).pipe(res);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
+import zlib from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+  const unzip = zlib.createUnzip();
+  const data = fs.createReadStream('./files/archive.gz');
+  const res = fs.createWriteStream('./files/fileToCompress.txt');
+
+  // pipeline(data, gzip, res);
+  data.pipe(unzip).pipe(res);
 };
 
 await decompress();
